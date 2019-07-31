5935ca5bdec5979bdfa8e412da429bf7c226e41a
William Greer <wgreer184@gmail.com>
2019-07-30 21:02:39 -0400

    Client establishes connection and receieves confirmation from server

--- node/src/main.rs
+++ node/src/main.rs
@@ -1,7 +1,15 @@
 //! This crate encloses the top-level circle node application.
 #![deny(warnings)]
 
+use failure::Error;
+use websocket::ClientBuilder;
+
+// String constant that holds coordinator address URL.
+static COORDINATOR_URL: &str = "ws://0.0.0.0:9090";
+
 // Main thread running the circle node.
-fn main() {
-    println!("Circle node initialized");
+fn main() -> Result<(), Error> {
+    let mut client = ClientBuilder::new(COORDINATOR_URL)?.connect_insecure()?;
+    println!("{:?}", client.recv_message()?);
+    Ok(())
 }
